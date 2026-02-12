9919b637fe7319dab66095c9b888661afb99d53d
Murilo Marinho <murilo.marinho@manchester.ac.uk>
2026-02-12 21:31:25 +0000

    [requirements] Adding file and removing installation from jupyter notebooks.

--- build_html.sh
+++ build_html.sh
@@ -3,12 +3,14 @@
 # update
 sudo apt-get update && sudo apt-get upgrade -y
 
+# requirements
+python -m pip install -r requirements.txt
+
 # While 2.0 is not stable
 python -m pip install jupyter-book --pre
 
 # Otherwise the links will not be correctly set up for th webpage
 export BASE_URL="https://marinholab.github.io/oxbr_dq_robotics_lessons"
 
-# Run once without the devel
 cat myst.yml
 python -m jupyter book build --html --execute
